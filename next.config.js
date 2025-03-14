eb6b91f0d52864fc54c86371bcb18bd94c1c9170
Souphiane Jender <85786227+SouphianeJ@users.noreply.github.com>
2025-03-14 13:23:39 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-
-module.exports = {
-reactStrictMode: true
+const nextConfig = {
+  reactStrictMode: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
   },
 };
 
+module.exports = nextConfig;
