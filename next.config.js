a4ed9ae05a78f6d1b80a9d0465cc7710945a44cb
Souphiane Jender <85786227+SouphianeJ@users.noreply.github.com>
2025-03-16 22:05:09 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-    experimental: {
-        appDir: 
+    experimental: 
+        appDir: false
     },
+   
     reactStrictMode: true,
     webpack(config) {
       config.resolve.fallback = { fs: false };
